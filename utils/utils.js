877dd1eb6ace20b39aa485cd69cea7221729d964
Mosleymos - MKE <keitamori@gmail.com>
2017-06-25 20:55:39 +0200

    Some practical funtion and utils to reuse in javascript to improve

---
+++ utils/utils.js
@@ -0,0 +1,117 @@
+// Took from http://raganwald.com/2012/12/01/combinators-in-js.html
+// Splat
+
+// Exemple of usage:
+// var squareMap = splat(function (n) {
+//   return n*n
+// });
+//
+// squareMap([1, 2, 3, 4, 5])
+//   //=> [1, 4, 9, 16, 25]
+
+function splat(fn){
+  return function(list){
+    return Array.prototype.call(list,fn)
+  }
+}
+
+// var inventory = {
+//   apples: 0,
+//   oranges 144,
+//   eggs: 36
+// };
+//
+// get('oranges')(inventory)
+//   //=> 144
+
+function get(attr){
+  return function(objet){
+     return object[attr]
+  }
+}
+
+// Combination
+// var inventories = [
+//   { apples: 0, oranges: 144, eggs: 36 },
+//   { apples: 240, oranges: 54, eggs: 12 },
+//   { apples: 24, oranges: 12, eggs: 42 }
+// ];
+//
+// splat(get('oranges'))(inventories)
+//   //=> [ 144, 54, 12 ]
+//
+
+
+// pluck('eggs')(inventories)
+//   //=> [ 36, 12, 42 ]
+//
+
+function pluck (attr) {
+  return splat(get(attr))
+}
+
+
+// maybe function or monae => to think about
+
+// Exemple of uses
+//
+// function checksForSomething = maybe(function (value) {
+//   // function's true logic
+// });
+//
+// var something = maybe(doesntCheckForSomething)(value);
+//
+// // Ultimage usage
+// arrayOfObjects.map(maybe(get('name')))
+//
+
+function maybe (fn) {
+  return function () {
+    var i;
+
+    if (arguments.length === 0) {
+      return
+    }
+    else {
+      for (i = 0; i < arguments.length; ++i) {
+        if (arguments[i] == null) return arguments[i]
+      }
+      return fn.apply(this, arguments)
+    }
+  }
+}
+
+// kestrel
+// Faire des side effects avec des varibles et fonctions mais retourne toujours la variable d'entree
+// exemple
+// log = kestrel(4)(console.log('hello'))
+// => hello
+// => 4
+// la variable log === 4
+var kestrel = function(x){
+  return function(y){
+    return x
+  }
+}
+
+// Version simple
+var log = function(something){
+  console.log(something)
+  return something
+}
+
+// Version avec kestrel
+var log = function(something){
+  return kestrel(something)(console.log(something))
+}
+
+// Simple assert
+// assert((1 === 1))
+var assert = function(condition){
+  return condition ? true : false
+}
+
+// Apply a function and one argument
+var applyOneArgumentOneFunction(fn,arg1){
+  return fn(arg1)
+}
