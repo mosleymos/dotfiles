b1b42fafd4f1aa58f49cdd67de14b034ce43b526
Mosleymos - MKE <keitamori@gmail.com>
2018-08-11 21:07:15 +0200

    Aucune décomposition du zshrc

--- zsh_modules/zsh_variables.zsh
+++
@@ -1,7 +0,0 @@
-# TO separate to different files
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-export EDITOR=vim
-export VISUAL=vim
-export MYVIMRC=/home/mori/.vimrc
-export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33'
